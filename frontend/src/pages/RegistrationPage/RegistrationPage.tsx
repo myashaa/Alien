e92789603d36d4ab6c295b1e931e9a544e05b719
myashaa <mianto017@mail.ru>
2022-11-15 00:01:19 +0300

    Cделала компонент RegistrationForm.

--- frontend/src/pages/RegistrationPage/RegistrationPage.tsx
+++ frontend/src/pages/RegistrationPage/RegistrationPage.tsx
@@ -3,11 +3,13 @@ import baseStyles from "../../index.module.css";
 
 import { Footer } from "../../components/Footer/Footer";
 import { Header } from '../../components/Header/Header';
+import { RegistrationForm } from '../../components/RegistrationForm/RegistrationForm';
 
 export const RegistrationPage = () => {
   return (
     <div className={ baseStyles.page}>
       <Header />
+      <RegistrationForm />
       <Footer />
     </div>
   );
