5ac1374c8c467db5c52db927dc97d850569c2a2b
myashaa <mianto017@mail.ru>
2022-11-14 23:10:29 +0300

    Сделала LoginationForm.

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -3,11 +3,13 @@ import baseStyles from "../../index.module.css";
 
 import { Footer } from "../../components/Footer/Footer";
 import { Header } from '../../components/Header/Header';
+import { LoginationForm } from '../../components/LoginationForm/LoginationForm';
 
 export const LoginPage = () => {
   return (
     <div className={ baseStyles.page}>
       <Header />
+      <LoginationForm />
       <Footer />
     </div>
   );
