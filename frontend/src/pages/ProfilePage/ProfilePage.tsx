3d95460d5f5f718e21d62db47a2847423dba2eab
myashaa <mianto017@mail.ru>
2022-11-14 18:05:13 +0300

    Сделала footer.

--- frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import baseStyles from "../../index.module.css";
+
+import { Footer } from "../../components/Footer/Footer";
 import { Header } from '../../components/Header/Header';
 
 export const ProfilePage = () => {
   return (
-    <Header />
+    <div className={ baseStyles.page}>
+      <Header />
+      <Footer />
+    </div>
   );
 }
