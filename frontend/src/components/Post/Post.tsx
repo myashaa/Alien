1e0dbf153d4f27ed410116af22350b3c6787abe9
myashaa <mianto017@mail.ru>
2022-11-15 22:55:13 +0300

    Создала компонент Post.

---
+++ frontend/src/components/Post/Post.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styles from "./LoginationForm.module.css";
+import baseStyles from "../../index.module.css";
+
+export function LoginationForm() {
+  return (
+    <section className={baseStyles.container}>
+      <h2 className={baseStyles.visuallyHidden}>Форма авторизации</h2>
+      <form className={styles.loginForm} action="#" method="post">
+        <div className={`${ styles.loginInputWrapper } ${ baseStyles.formInputWrapper }`}>
+          <label className={`${styles.loginLabel} ${baseStyles.formLabel}`}>
+            Электронная почта
+          </label>
+          <div className={ baseStyles.formInputSection }>
+            <input className={`${styles.loginInput} ${baseStyles.formInput}`} id="login-email" type="email" name="email" placeholder="Укажите эл.почту" />
+            <button className={`${baseStyles.formErrorButton} ${baseStyles.button}`} type="button">!<span className={baseStyles.visuallyHidden}>Информация об ошибке</span></button>
+            <div className={baseStyles.formErrorText}>
+              <h3 className={baseStyles.formErrorTitle}>
+                Заголовок сообщения
+              </h3>
+              <p className={baseStyles.formErrorDesc}>
+                Текст сообщения об ошибке, подробно объясняющий, что не так.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={`${ styles.loginInputWrapper } ${ baseStyles.formInputWrapper }`}>
+          <label className={`${styles.loginLabel} ${baseStyles.formLabel}`}>
+            Пароль
+          </label>
+          <div className={ baseStyles.formInputSection }>
+            <input className={`${styles.loginInput} ${baseStyles.formInput}`} id="login-password" type="password" name="password" placeholder="Введите пароль" />
+            <button className={`${baseStyles.formErrorButton} ${baseStyles.button}`} type="button">!<span className={baseStyles.visuallyHidden}>Информация об ошибке</span></button>
+            <div className={baseStyles.formErrorText}>
+              <h3 className={baseStyles.formErrorTitle}>
+                Заголовок сообщения
+              </h3>
+              <p className={baseStyles.formErrorDesc}>
+                Текст сообщения об ошибке, подробно объясняющий, что не так.
+              </p>
+            </div>
+          </div>
+        </div>
+        <button className={`${styles.loginSubmit} ${baseStyles.button} ${baseStyles.buttonMain}`} type="submit">
+          Отправить
+        </button>
+      </form>
+    </section>
+  );
+}
