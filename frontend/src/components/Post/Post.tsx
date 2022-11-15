002f4bafcd8a25de3fd9276bb26c26ddea37d56a
myashaa <mianto017@mail.ru>
2022-11-16 00:39:49 +0300

    Сделала компонент Post.

--- frontend/src/components/Post/Post.tsx
+++ frontend/src/components/Post/Post.tsx
@@ -1,50 +1,63 @@
 import React from "react";
-import styles from "./LoginationForm.module.css";
+import styles from "./Post.module.css";
 import baseStyles from "../../index.module.css";
 
-export function LoginationForm() {
+import Photo from "../../img/rock-medium.jpg";
+import UserPhoto from "../../img/userpic-larisa-small.jpg";
+import IconHeart from "../../img/icon-heart.svg"; 
+import IconHeartActive from "../../img/icon-heart-active.svg"; 
+import IconComment from "../../img/icon-comment.svg"; 
+
+export function Post() {
   return (
-    <section className={baseStyles.container}>
-      <h2 className={baseStyles.visuallyHidden}>Форма авторизации</h2>
-      <form className={styles.loginForm} action="#" method="post">
-        <div className={`${ styles.loginInputWrapper } ${ baseStyles.formInputWrapper }`}>
-          <label className={`${styles.loginLabel} ${baseStyles.formLabel}`}>
-            Электронная почта
-          </label>
-          <div className={ baseStyles.formInputSection }>
-            <input className={`${styles.loginInput} ${baseStyles.formInput}`} id="login-email" type="email" name="email" placeholder="Укажите эл.почту" />
-            <button className={`${baseStyles.formErrorButton} ${baseStyles.button}`} type="button">!<span className={baseStyles.visuallyHidden}>Информация об ошибке</span></button>
-            <div className={baseStyles.formErrorText}>
-              <h3 className={baseStyles.formErrorTitle}>
-                Заголовок сообщения
-              </h3>
-              <p className={baseStyles.formErrorDesc}>
-                Текст сообщения об ошибке, подробно объясняющий, что не так.
-              </p>
+    <article className={ styles.post }>
+      <header className={styles.postHeader}>
+        <h2><a href="#">Наконец, обработал фотки!</a></h2>
+      </header>
+      <div className={styles.postMain}>
+        <div className={styles.postPhotoImageWrapper}>
+          <img src={Photo} alt="Фото от пользователя" width="360" height="240" />
+        </div>
+        <p>
+          Озеро Байкал – огромное древнее озеро в горах Сибири к северу от монгольской границы. Байкал
+          считается самым глубоким озером в мире. Он окружен сетью пешеходных маршрутов, называемых
+          Большой байкальской тропой. Деревня Листвянка, расположенная на западном берегу озера, –
+          популярная отправная точка для летних экскурсий. Зимой здесь можно кататься на коньках и
+          собачьих упряжках.
+        </p>
+        <div className={styles.postTextMoreLinkWrapper}>
+            <a className={styles.postTextMoreLink} href="#">Читать далее</a>
+        </div>
+      </div>
+      <footer className={styles.postFooter}>
+        <div className={styles.postAuthor}>
+          <a className={styles.postAuthorLink} href="#" title="Автор">
+            <div className={styles.postAvatarWrapper}>
+              <img className={styles.postAuthorAvatar} src={UserPhoto}
+                alt="Аватар пользователя" />
             </div>
+            <div className={styles.postInfo}>
+              <b className={ styles.postAuthorName}>Лариса Роговая</b>
+              <time className={styles.postTime}>Месяц назад</time>
+            </div>
+          </a>
+        </div>
+        <div className={styles.postIndicators}>
+          <div className={styles.postButtons}>
+            <a className={`${styles.postIndicator} ${styles.postIndicatorLikes} ${baseStyles.button}`} href="#" title="Лайк">
+              <IconHeart className={styles.postIndicatorIcon} width="20" height="17" />
+              {/* <IconHeartActive className={`${ styles.postIndicatorIcon } ${ styles.postIndicatorIconLikeActive }`} width="20" height="17"/> */}
+              <span>250</span>
+              <span className={baseStyles.visuallyHidden}>количество лайков</span>
+            </a>
+            <a className={`${styles.postIndicator} ${styles.postIndicatorComments} ${baseStyles.button}`} href="#" title="Комментарии">
+              <IconComment className={ styles.postIndicatorIcon } width="19" height="17" />
+              <span>25</span>
+              <span className={baseStyles.visuallyHidden}>количество комментариев</span>
+            </a>
           </div>
         </div>
-        <div className={`${ styles.loginInputWrapper } ${ baseStyles.formInputWrapper }`}>
-          <label className={`${styles.loginLabel} ${baseStyles.formLabel}`}>
-            Пароль
-          </label>
-          <div className={ baseStyles.formInputSection }>
-            <input className={`${styles.loginInput} ${baseStyles.formInput}`} id="login-password" type="password" name="password" placeholder="Введите пароль" />
-            <button className={`${baseStyles.formErrorButton} ${baseStyles.button}`} type="button">!<span className={baseStyles.visuallyHidden}>Информация об ошибке</span></button>
-            <div className={baseStyles.formErrorText}>
-              <h3 className={baseStyles.formErrorTitle}>
-                Заголовок сообщения
-              </h3>
-              <p className={baseStyles.formErrorDesc}>
-                Текст сообщения об ошибке, подробно объясняющий, что не так.
-              </p>
-            </div>
-          </div>
-        </div>
-        <button className={`${styles.loginSubmit} ${baseStyles.button} ${baseStyles.buttonMain}`} type="submit">
-          Отправить
-        </button>
-      </form>
-    </section>
+      </footer>
+    </article>
   );
 }
--- frontend/src/pages/FeedPage/FeedPage.tsx
+++ frontend/src/pages/FeedPage/FeedPage.tsx
@@ -3,11 +3,13 @@ import baseStyles from "../../index.module.css";
 
 import { Footer } from "../../components/Footer/Footer";
 import { Header } from '../../components/Header/Header';
+import { Post } from '../../components/Post/Post';
 
 export const FeedPage = () => {
   return (
     <div className={ baseStyles.page}>
       <Header />
+      <Post />
       <Footer />
     </div>
   );
