b7752ce579d5e6f5599986f17ca9a696fa7d9707
myashaa <mianto017@mail.ru>
2022-11-16 01:26:58 +0300

    Сделала страницу my-feed.

--- frontend/src/pages/MyFeedPage/MyFeedPage.tsx
+++ frontend/src/pages/MyFeedPage/MyFeedPage.tsx
@@ -1,13 +1,60 @@
 import React from "react";
+import styles from "./MyFeedPage.module.css";
 import baseStyles from "../../index.module.css";
 
 import { Footer } from "../../components/Footer/Footer";
 import { Header } from '../../components/Header/Header';
+import { Post } from '../../components/Post/Post';
+
+import IconSort from "../../img/icon-sort.svg"; 
 
 export const MyFeedPage = () => {
   return (
-    <div className={ baseStyles.page}>
+    <div className={`${baseStyles.page} ${styles.pageFeed}`}>
       <Header />
+      <div className={styles.mainWrapper}>
+        <div className={baseStyles.container}>
+          <h1 className={`${baseStyles.pageTitle} ${baseStyles.pageTitleLogin}`}>
+            Лента
+          </h1>
+        </div>
+        <div className={baseStyles.container}>
+          <div className={styles.feedFiltersWrapper}>
+            <div className={styles.feedSorting}>
+              <b className={styles.feedSortingCaption}>Сортировка:</b>
+              <ul className={styles.feedSortingList}>
+                <li className={`${ styles.sortingItem } ${ styles.sortingItemFeed }`}>
+                  <a className={`${ styles.sortingLink } ${ styles.sortingLinkActive }`} href="#">
+                    <span>Популярность</span>
+                    <IconSort className={styles.sortingIcon} width="10" height="12" />
+                  </a>
+                </li>
+                <li className={ styles.sortingItem }>
+                  <a className={ styles.sortingLink } href="#">
+                    <span>Дата</span>
+                  </a>
+                </li>
+                <li className={styles.sortingItem} >
+                  <a className={ styles.sortingLink } href="#">
+                    <span>Лайки</span>
+                  </a>
+                </li>
+                <li className={ styles.sortingItem }>
+                  <a className={ styles.sortingLink } href="#">
+                    <span>Комментарии</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className={`${ styles.feedPosts } ${ baseStyles.container }`}>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </div>  
+      </div>
       <Footer />
     </div>
   );
