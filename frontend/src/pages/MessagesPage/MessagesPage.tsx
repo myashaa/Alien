c17d22ac3abf616a35c7068148e126b97c8522a5
myashaa <mianto017@mail.ru>
2022-11-17 00:36:35 +0300

    Сделала компонент Dialog.

--- frontend/src/pages/MessagesPage/MessagesPage.tsx
+++ frontend/src/pages/MessagesPage/MessagesPage.tsx
@@ -4,12 +4,14 @@ import baseStyles from "../../index.module.css";
 import { Footer } from "../../components/Footer/Footer";
 import { Header } from '../../components/Header/Header';
 import { Message } from '../../components/Message/Message';
+import { Dialog } from '../../components/Dialog/Dialog';
 
 export const MessagesPage = () => {
   return (
     <div className={ baseStyles.page}>
       <Header />
       <Message />
+      <Dialog />
       <Footer />
     </div>
   );
