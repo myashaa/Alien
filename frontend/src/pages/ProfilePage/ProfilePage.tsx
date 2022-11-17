3bbd18661929e2013975a3620c831cd85e062fbc
Диана Абушаева <abu13.di@gmail.com>
2022-11-17 18:54:37 +0300

    Новая ветка для профиля. Заголовок профиля на страницу профиля.

--- frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -3,11 +3,13 @@ import baseStyles from "../../index.module.css";
 
 import { Footer } from "../../components/Footer/Footer";
 import { Header } from '../../components/Header/Header';
+import { Profile } from '../../components/Profile/Profile';
 
 export const ProfilePage = () => {
   return (
     <div className={ baseStyles.page}>
       <Header />
+      <Profile/>
       <Footer />
     </div>
   );
