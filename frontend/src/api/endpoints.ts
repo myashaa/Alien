b09e4b48ce3f35e05fb64147428e106762a9d2ba
Диана Абушаева <abu13.di@gmail.com>
2023-01-26 15:10:55 +0300

    Собираюсь все сломать.

--- frontend/src/api/endpoints.ts
+++ frontend/src/api/endpoints.ts
@@ -1,6 +1,6 @@
 const Endpoints = {
     AUTH: {
-      LOGIN: 'http://localhost:44390/api/auth/login/',
+      LOGIN: 'https://localhost:44390/api/auth/login/',
       REFRESH: '/refresh',
       LOGOUT: '/logout',
       PROFILE: '/profile',
